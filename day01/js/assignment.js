d9f690eb1fa8bcd2ce0cf1b291a5e36c36b2128a
jsher99 <jeremyjsherman@gmail.com>
2016-11-01 14:46:13 -0700

    day01 assignment
    I have questions between running console.log with two variables in ()
    versus using .concat of two variables. How does the console see each
    one? How are they different?

---
+++ day01/js/assignment.js
@@ -0,0 +1,51 @@
+// Jeremy Sherman 11/01/16 Javascript 1
+
+// day01 Assignment
+var num3 = 12,
+    num4 = 0x1E,
+    text2 = "Answer equals: ";
+
+var sum2 = (num3) + (num4);
+
+var answer = (text2).concat(sum2);
+
+console.log(answer);
+
+// (This was my first attempt) What is the different between this one and above? I added concatenate above on my second attempt but what is the result of this one below? How does the computer see each one in the console? Is this merely printing both var in the console log()? What does concatenate do? What are its future uses?
+var num1 = 12,
+    num2 = 0x1E,
+    text = "Answer equals: ";
+
+var sum = (num1) + (num2);
+
+console.log(text, sum);
+
+
+
+
+
+
+
+
+
+
+
+/*
+On the assignment.js file
+Create three variables.
+
+The first two should store the numbers:
+values 12 and 30
+(for an extra challenge... convert 30 into a hexadecimal)
+The third variable should store the string "Answer equals: "
+Add the first two numeric variables together using the + operator, save this to a new variable.
+Concatenate the string from your third variable with the number from your fourth (new) variable. Save this to a new variable called "answer".
+Console log answer.
+*/
+
+var str1 = "Hello ";
+var str2 = "world!";
+var str3 = " Have a nice day!";
+var res = str1.concat(str2, str3);
+
+console.log(res);
---
+++ day01/js/script.js
@@ -0,0 +1,105 @@
+//single line comment
+/*multi line comment
+
+Jeremy 
+Sherman*/
+
+// console.log("Hello World");
+
+//string variable
+var firstName = "Jeremy";
+// console.log(firstName);
+
+
+
+
+/*Strings*/
+var string1 = "this is a string"; // string literal
+var string2 = 'i\'ve done this before'; //mix and match quotes
+var string3 = new String("This is a string also"); // string object
+/*Numbers*/
+var number1 = 5; //integer 5
+var number2 = "5"; //string 5 <-- not a number!
+/*Floatss*/
+var float1 = 2.75;
+var float2 = 245.45678;
+/*Boolean... 1s and 0s, Binary*/
+var boolean1 = true;
+var boolean2 = false;
+/*Array*/
+var array1 = ['bread', 'eggs', 'milk'];
+var array2 = ['matt', 6.4, false]; //index 0, 1, 2 arrays are important to game
+// console.log(array2[1]); // output index 1 of array
+/*Object*/
+var object1 = {
+    name: 'matt',  //value and key to know what were looking for
+    height: '6.4',
+    caffinated: false
+};
+// console.log(object1.height); //input the key you want
+
+//Create two string variables: firstname & lastname
+//Create two number veriables: bday-month & bday-date
+//Create two float variables: height & weight
+//Create two boolean variables: gender & pets
+//Create two array variables: 3 fav colors & fav movies
+//Create two object variables: car & yourself (3 items)
+//Console each.
+
+var stringfn = "jeremy";
+var stringln = "sherman";
+var numberbdm = "07";
+var numberbdd = "04";
+var floath = 6.234;
+var floatw = 4000.123;
+var booleanmale = true;
+var array3favcolor = ['blue', 'blue', 'green'];
+var array3movies = ['matrix', 'lotr', 'terminator'];
+var objectcar = {
+    name: 'honda',
+    model: 'civic',
+    year: '2006'    
+};
+var objectmyself = {
+    name: 'jeremy',
+    age: '29',
+    face: 'beard'
+};
+
+console.log(stringfn);
+console.log(stringln);
+console.log(numberbdm);
+console.log(numberbdd);
+console.log(floath);
+console.log(floatw);
+console.log(booleanmale);
+console.log(array3favcolor);
+console.log(array3movies);
+console.log(objectcar.name);
+console.log(objectcar.model);
+console.log(objectcar.year);
+console.log(objectmyself.name);
+console.log(objectmyself.age);
+console.log(objectmyself.face);
+
+
+// type of keyword call
+console.log(typeof boolean1);
+console.log(typeof array1);
+console.log(typeof number1);
+
+
+
+
+/*On the assignment.js file
+Create three variables.
+The first two should store the numbers: values 12 and 30 
+(for an extra challenge... conver 30 into a hexdecimal)
+The third variable should store the string "Answer equals: "
+Add the first two numberic variables together using the + operator, save this to a new variable.
+Concatenate the string from your third variable with the number from your fourth (new) variable.
+Save this to a new variable called "answer".
+Console log answer.
+*/
+
+
